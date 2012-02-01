4eb47de2633a8b0e27647adcd8cd4d1fbb3c463b
Michael Brown <fractalmbrown@gmail.com>
2012-02-01 13:24:06 -0500

    Added About box stub, changed some app settings

--- src/com/speedacm/treeview/MainMenuActivity.java
+++ src/com/speedacm/treeview/MainMenuActivity.java
@@ -6,15 +6,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.speedacm.treeview.menu.ActivityStarter;
 import com.speedacm.treeview.menu.MenuActionListener;
 import com.speedacm.treeview.menu.MenuEntry;
+import com.speedacm.treeview.views.AboutActivity;
 
 public class MainMenuActivity extends Activity implements OnItemClickListener
 {	
@@ -26,8 +27,6 @@ public class MainMenuActivity extends Activity implements OnItemClickListener
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        // TODO: perhaps remove the title bar globally via a theme: declaration in XML
-        requestWindowFeature(Window.FEATURE_NO_TITLE); // remove title bar
         setContentView(R.layout.mainmenu);
         
         addMenuItems();
@@ -67,7 +66,7 @@ public class MainMenuActivity extends Activity implements OnItemClickListener
 		menuEntries.add(new MenuEntry(getString(R.string.mmenu_plantfacts), todoHandler));
 		menuEntries.add(new MenuEntry(getString(R.string.mmenu_wildfacts), todoHandler));
 		menuEntries.add(new MenuEntry(getString(R.string.mmenu_scavhunt), todoHandler));
-		menuEntries.add(new MenuEntry(getString(R.string.mmenu_about), todoHandler));
+		menuEntries.add(new MenuEntry(getString(R.string.mmenu_about), new ActivityStarter(this, AboutActivity.class)));
 		menuEntries.add(new MenuEntry(getString(R.string.mmenu_creds), todoHandler));
 	}
 }
