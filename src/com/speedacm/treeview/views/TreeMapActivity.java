83ce278b766df0cf90988c34225c9b85a4978f0d
Michael Brown <fractalmbrown@gmail.com>
2012-02-06 11:10:40 -0500

    Added permissions, makes map to zoom to UofL

--- src/com/speedacm/treeview/views/TreeMapActivity.java
+++ src/com/speedacm/treeview/views/TreeMapActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 import com.speedacm.treeview.R;
@@ -20,12 +21,20 @@ public class TreeMapActivity extends MapActivity
 	private boolean mFetchedMap = false;
 	private MapView mMap;
 	
+	// latitude and longitude of "University of Louisville"
+	// according to google: 38.2159018, -85.7581278
+	
+	private final GeoPoint mUofL_LatLong = new GeoPoint(38215901, -85758128);
+	private final int mUofL_DefZoom = 18;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.treemap);
 		mMap = (MapView)findViewById(R.id.mainTreeMap);
+		mMap.getController().setCenter(mUofL_LatLong);
+		mMap.getController().setZoom(mUofL_DefZoom);
 	}
 	
 	/** This function is only called once, before the menu is shown */
