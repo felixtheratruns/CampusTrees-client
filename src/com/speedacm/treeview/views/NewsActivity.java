417d3d8a68665542402f066b90729ac72ab1103b
Joel Cambon <jlcamb02@louisville.edu>
2012-02-05 19:17:07 -0500

    Added files for main menu item layouts, strings, and activity

---
+++ src/com/speedacm/treeview/views/NewsActivity.java
@@ -0,0 +1,15 @@
+package com.speedacm.treeview.views;
+
+import com.speedacm.treeview.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewsActivity extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.newsbox);
+	}
+}
---
+++ src/com/speedacm/treeview/views/PlantFactsActivity.java
@@ -0,0 +1,15 @@
+package com.speedacm.treeview.views;
+
+import com.speedacm.treeview.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlantFactsActivity extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.plantfactsbox);
+	}
+}
