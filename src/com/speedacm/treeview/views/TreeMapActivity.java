ea7a603167692c94390fbc4a780adba295e663a7
Michael Brown <fractalmbrown@gmail.com>
2012-02-06 10:10:43 -0500

    Added basic map to view

--- src/com/speedacm/treeview/views/TreeMapActivity.java
+++ src/com/speedacm/treeview/views/TreeMapActivity.java
@@ -1,13 +1,14 @@
 package com.speedacm.treeview.views;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 import com.speedacm.treeview.R;
 
-public class TreeMapActivity extends Activity
+public class TreeMapActivity extends MapActivity
 {
 	
 	//
@@ -17,12 +18,14 @@ public class TreeMapActivity extends Activity
 	//
 	private boolean mFetchedTreeTypes = false;
 	private boolean mFetchedMap = false;
+	private MapView mMap;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.treemap);
+		mMap = (MapView)findViewById(R.id.mainTreeMap);
 	}
 	
 	/** This function is only called once, before the menu is shown */
@@ -43,5 +46,11 @@ public class TreeMapActivity extends Activity
 			// TODO: fetch trees from server and use menu.add
 		}
 		return true;
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
