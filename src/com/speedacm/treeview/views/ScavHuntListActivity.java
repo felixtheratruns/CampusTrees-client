cc71fa1e1d20285efa836cae1433c2479ac8f038
Joel <joel2.718@gmail.com>
2012-08-29 13:03:51 -0400

    fixed problem in scavenger hunt where different scavenger hunts would have the same items saved as checked if they had the same title

--- src/com/speedacm/treeview/views/ScavHuntListActivity.java
+++ src/com/speedacm/treeview/views/ScavHuntListActivity.java
@@ -38,7 +38,9 @@ public class ScavHuntListActivity extends ListActivity {
 	final String SETTING_TODOLIST = "todolist";
 
 	private ArrayList<String> selectedItems = new ArrayList<String>();
-
+	
+	private String scavHuntTitle = "";
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -50,6 +52,8 @@ public class ScavHuntListActivity extends ListActivity {
 		System.out.println("the position is: "+ position);
 		ScavHunt s_hunt = getIntent().getParcelableExtra("s_hunt");
 		//System.out.println("body string: "+ s_hunt.getBody().toString());
+		
+		scavHuntTitle = s_hunt.getTitle();
 		
 		
 
@@ -137,9 +141,9 @@ public class ScavHuntListActivity extends ListActivity {
 	private void LoadSelections() {
 		// if the selections were previously saved load them
 		SharedPreferences settingsActivity = getPreferences(MODE_PRIVATE);
-		if (settingsActivity.contains(SETTING_TODOLIST)) {
+		if (settingsActivity.contains(scavHuntTitle+SETTING_TODOLIST)) {
 			String savedItems = settingsActivity
-					.getString(SETTING_TODOLIST, "");
+					.getString(scavHuntTitle+SETTING_TODOLIST, "");
 
 			this.selectedItems.addAll(Arrays.asList(savedItems.split(",")));
 			int count = this.mainListView.getAdapter().getCount();
@@ -159,7 +163,7 @@ public class ScavHuntListActivity extends ListActivity {
 		SharedPreferences settingsActivity = getPreferences(MODE_PRIVATE);
 		SharedPreferences.Editor prefEditor = settingsActivity.edit();
 		String savedItems = getSavedItems();
-		prefEditor.putString(SETTING_TODOLIST, savedItems);
+		prefEditor.putString(scavHuntTitle+SETTING_TODOLIST, savedItems);
 		prefEditor.commit();
 	}
 
