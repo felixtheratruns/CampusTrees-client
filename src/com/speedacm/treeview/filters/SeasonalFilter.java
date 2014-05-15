2c6680b3362dbf84c0c61d3c98ca1381744912a2
joel <joel2.718@gmail.com>
2014-05-15 01:42:13 -0400

    added license attached to files

--- src/com/speedacm/treeview/filters/SeasonalFilter.java
+++ src/com/speedacm/treeview/filters/SeasonalFilter.java
@@ -1,3 +1,5 @@
+/** This file is part of client-side of the CampusTrees Project. 
+It is subject to the license terms in the LICENSE file found in the top-level directory of this distribution. No part of CampusTrees Project, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the LICENSE file.*/
 package com.speedacm.treeview.filters;
 
 import java.util.Calendar;
